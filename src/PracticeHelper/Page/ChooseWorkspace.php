ac77bfd5d12e3499033c9120f621200db1092d6a
Gregory Beaver <greg@chiaraquartet.net>
2014-09-06 22:26:01 -0500

    remove debug code, return a value from toString

--- src/PracticeHelper/Page/ChooseWorkspace.php
+++ src/PracticeHelper/Page/ChooseWorkspace.php
@@ -6,15 +6,14 @@ class ChooseWorkspace
     protected $main;
     function __construct(P $main)
     {
-        var_dump($main);
         $this->main = $main;
     }
 
     function __toString()
     {
-        var_dump($this->main->workspaces);
         foreach ($this->main->workspaces->matching('Lessons') as $space) {
             $ret .= new Button($space->title, $space->id, 'workspace');
         }
+        return $ret;
     }
 }
