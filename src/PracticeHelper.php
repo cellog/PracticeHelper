b81065e0e0ac7be7c7eae598bfad8d5182237160
Gregory Beaver <greg@chiaraquartet.net>
2014-09-07 11:40:49 -0500

    debug

--- src/PracticeHelper.php
+++ src/PracticeHelper.php
@@ -62,7 +62,7 @@ class PracticeHelper
         }
         $workspace = new Chiara\PodioWorkspace($id);
         foreach ($workspace->apps as $potential) {
-        var_dump($potential->title);
+        var_dump($potential);exit;
             if ($potential->title == 'Practicing') {
                 $_SESSION['practicing']['app'] = $potential->id;
             }
